2a2373f5dacadfab5cfe4c0ada66bdf030778f8e
R.Minachev <vardol@mail.ru>
2022-12-02 23:52:55 +0300

    added random generation to zadacha01.py

--- HW4/zadacha01.py
+++ HW4/zadacha01.py
@@ -9,6 +9,14 @@ def list_summ(mylist: list):
         result += mylist[i]
     return result
 
-numbers_list = [2, 3, 5, 9, 3]
+def fill_array_random(input_array: list, min = -10, max = 10):
+    import random
+    for i in range(0,len(input_array)):
+        input_array[i] = random.randint(min,max)
+    return input_array
 
+numbers_list = [0] * 5
+numbers_list = fill_array_random(numbers_list)
+
+print(numbers_list)
 print(list_summ(numbers_list))
